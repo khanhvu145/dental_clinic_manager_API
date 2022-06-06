3e22d8be83f2cbaa6d53d1fcd1cef6062200ba61
khanhvu145 <khanhvn@twin.vn>
2022-06-06 15:52:05 +0700

    update

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -3,9 +3,8 @@ const Employee = require('../models/Employee');
 const EmployeeController = {
     getListEmployee: async(req, res) => {
         try {
-            // const employees = await Employee.find({_id: req.body.id});
-            await Employee.findByIdAndRemove(req.body.id);
-            res.status(200).json("true");
+            const employees = await Employee.find({});
+            res.status(200).json(employees);
         }catch(err){
             res.status(500).json(err)
         }
