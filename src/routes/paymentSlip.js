084838165ee17713e74fdfa842689072b0179aa1
khanhvu145 <khanhvn@twin.vn>
2023-12-17 18:17:15 +0700

    update API

--- src/routes/paymentSlip.js
+++ src/routes/paymentSlip.js
@@ -11,8 +11,72 @@ router.post('/export', GetAccessToken('payment', 'export'), PaymentSlipControlle
 router.post('/cancel', GetAccessToken('payment', 'cancel'), PaymentSlipController.cancel);
 router.post('/updateOriginalDocuments', GetAccessToken('payment', 'update'), PaymentSlipController.updateOriginalDocuments);
 router.post('/complete', GetAccessToken('payment', 'update'), PaymentSlipController.complete);
+/**
+ * @openapi
+ * '/api/paymentSlip/getById/{id}':
+ *  get:
+ *     tags:
+ *      - PaymentSlip
+ *     summary: Get payment slip by query
+ *     security:
+ *      - bearerAuth: []
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        type: objectId
+ *        required: true
+ *        example: 630a038c4b0253de39d18206
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Error
+ *      403:
+ *        description: Invalid token / Not have access
+ *      500:
+ *        description: Server error
+ */
 router.get('/getById/:id/', verifyToken, PaymentSlipController.getById);
 router.post('/create', GetAccessToken('payment', 'create'), PaymentSlipController.create);
+/**
+ * @openapi
+ * '/api/paymentSlip/getByQuery':
+ *  post:
+ *     tags:
+ *      - PaymentSlip
+ *     summary: Get payment slip by query
+ *     security:
+ *      - bearerAuth: []
+ *     requestBody:
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            properties:
+ *              filters:
+ *                type: object
+ *                example:
+ *                  dateF: []
+ *                  statusF: 'all'
+ *                  codeF: ''
+ *              sorts:
+ *                type: string
+ *                example: -1
+ *              pages:
+ *                type: object
+ *                example: 
+ *                  from: 0
+ *                  size: 10
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Error
+ *      403:
+ *        description: Invalid token / Not have access
+ *      500:
+ *        description: Server error
+ */
 router.post('/getByQuery', GetAccessToken('payment', 'view'), PaymentSlipController.getByQuery);
 
 module.exports = router;
--- src/routes/receipts.js
+++ src/routes/receipts.js
@@ -7,7 +7,73 @@ const router = express.Router();
 router.post('/export', GetAccessToken('receipts', 'export'), receiptsController.export);
 router.post('/cancel', GetAccessToken('receipts', 'cancelReceipts'), receiptsController.cancel);
 router.post('/getReceiptsByPaymentId', verifyToken, receiptsController.getReceiptsByPaymentId);
+/**
+ * @openapi
+ * '/api/receipts/getById/{id}':
+ *  get:
+ *     tags:
+ *      - Receipts
+ *     summary: Get receipts by query
+ *     security:
+ *      - bearerAuth: []
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        type: objectId
+ *        required: true
+ *        example: 630a038c4b0253de39d18206
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Error
+ *      403:
+ *        description: Invalid token / Not have access
+ *      500:
+ *        description: Server error
+ */
 router.get('/getById/:id/', verifyToken, receiptsController.getById);
+/**
+ * @openapi
+ * '/api/receipts/getByQuery':
+ *  post:
+ *     tags:
+ *      - Receipts
+ *     summary: Get receipts by query
+ *     security:
+ *      - bearerAuth: []
+ *     requestBody:
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            properties:
+ *              filters:
+ *                type: object
+ *                example:
+ *                  customerF: ''
+ *                  dateF: []
+ *                  examinationCodeF: ''
+ *                  statusF: 'all'
+ *                  codeF: ''
+ *              sorts:
+ *                type: string
+ *                example: -1
+ *              pages:
+ *                type: object
+ *                example: 
+ *                  from: 0
+ *                  size: 10
+ *     responses:
+ *      200:
+ *        description: Successfully
+ *      400:
+ *        description: Error
+ *      403:
+ *        description: Invalid token / Not have access
+ *      500:
+ *        description: Server error
+ */
 router.post('/getByQuery', GetAccessToken('receipts', 'view'), receiptsController.getByQuery);
 
 module.exports = router;
